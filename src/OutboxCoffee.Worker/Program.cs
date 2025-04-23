11126ad848bcc9883493aa7c37c6d782044a55a2
Ricardo Fulgencio Alves <ricardo.fulgencio@hotmail.com>
2025-04-23 14:40:45 -0300

    chore: Ajustes projeto

---
+++ src/OutboxCoffee.Worker/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OutboxCoffee.Worker.Processors;
+using OutboxCoffee.Worker.Steps;
+using WorkflowCore.Interface;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddLogging();
+        services.AddWorkflow();
+
+        services.AddTransient<ReadOutboxStep>();
+        services.AddTransient<PublishEventStep>();
+        services.AddTransient<MarkAsProcessedStep>();
+
+        services.AddTransient<IWorkflow, OutboxDispatchWorkflow>();
+    })
+    .Build();
+
+var workflowHost = host.Services.GetRequiredService<IWorkflowHost>();
+workflowHost.RegisterWorkflow<OutboxDispatchWorkflow>();
+workflowHost.Start();
+
+Console.WriteLine("Outbox Worker running. Press Enter to exit...");
+Console.ReadLine();
+workflowHost.Stop();
